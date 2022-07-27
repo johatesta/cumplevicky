92d6dceb515648d92dbd2646981f7bb599e8ea59
johatesta <johatesta@gmail.com>
2022-07-27 20:22:50 -0300

    primero

---
+++ js/main.js
@@ -0,0 +1,84 @@
+let launchDate = new Date("Jul 29, 2022 12:00:00").getTime();
+
+// Setup Timer to tick every 1 second
+let timer = setInterval(tick, 1000);
+
+function tick () {
+  // Get current time
+  let now = new Date().getTime();
+  // Get the difference in time to get time left until reaches 0
+  let t = launchDate - now;
+
+  // Check if time is above 0
+  if (t > 0) {
+    // Setup Days, hours, seconds and minutes
+    // Algorithm to calculate days...
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    // prefix any number below 10 with a "0" E.g. 1 = 01
+    if (days < 10) { days = "0" + days; }
+    
+    // Algorithm to calculate hours
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if (hours < 10) { hours = "0" + hours; }
+
+    // Algorithm to calculate minutes
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    if (mins < 10) { mins = "0" + mins; }
+
+    // Algorithm to calc seconds
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+    if (secs < 10) { secs = "0" + secs; }
+
+    // Create Time String
+    let time = `${days} : ${hours} : ${mins} : ${secs}`;
+
+    // Set time on document
+    document.querySelector('.countdown').innerText = time;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
---
+++ js/script.js
@@ -0,0 +1,109 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () =>{
+  themeToggler.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
+
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+  
+  btn.onclick = () =>{
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
+
+});
+
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop:true,
+  autoplay:{
+    delay: 3000,
+    disableOnInteraction:false,
+  }
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop:true,
+    spaceBetween: 10,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1050: {
+        slidesPerView: 3,
+      },    
+    },
+    autoplay:{
+      delay: 5000,
+      disableOnInteraction:false,
+  }
+});
+
+let launchDate = new Date("Jul 29, 2022 12:00:00").getTime();
+
+// Setup Timer to tick every 1 second
+let timer = setInterval(tick, 1000);
+
+function tick () {
+  // Get current time
+  let now = new Date().getTime();
+  // Get the difference in time to get time left until reaches 0
+  let t = launchDate - now;
+
+  // Check if time is above 0
+  if (t > 0) {
+    // Setup Days, hours, seconds and minutes
+    // Algorithm to calculate days...
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    // prefix any number below 10 with a "0" E.g. 1 = 01
+    if (days < 10) { days = "0" + days; }
+    
+    // Algorithm to calculate hours
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if (hours < 10) { hours = "0" + hours; }
+
+    // Algorithm to calculate minutes
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    if (mins < 10) { mins = "0" + mins; }
+
+    // Algorithm to calc seconds
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+    if (secs < 10) { secs = "0" + secs; }
+
+    // Create Time String
+    let time = `${days} : ${hours} : ${mins} : ${secs}`;
+
+    // Set time on document
+    document.querySelector('.countdown').innerText = time;
+  }
+}
